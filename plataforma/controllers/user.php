0e93f82436101ee9ef3402c1244d4aea7f03b9b2
Jose Moreno <Masizo@Air-de-Jose.lan>
2015-05-13 23:23:25 -0600

    Creacion del controlador user

---
+++ plataforma/controllers/user.php
@@ -0,0 +1,13 @@
+<?php
+class Admin extends CI_Controller {
+
+    function __construct()
+	{
+	    parent::__construct();
+		$this->load->library('grocery_CRUD');
+		$this->load->library('encrypt');		
+		$this->load->model('usuarios_model');
+   	}
+
+}
+?>
