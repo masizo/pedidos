496457d575c8308c70be902709e9e9364613cf87
Jose Moreno <Masizo@MacBook-Air-de-Jose.local>
2015-05-17 18:16:12 -0600

    arreglando vista registro

--- plataforma/views/registro.php
+++ plataforma/views/registro.php
@@ -1,52 +1,65 @@
-<html>
-	<head>
-			<title>::: Pedidos Nayarit :::</title>
-	</head>
-<body>
 
-<h1> Registro de Usuario </h1>
+<div class="row">
+	<div class="col-md-2 col-md-offset-7 derecha">
+		<!--  inicio formulario   -->
+		<?php
+			echo $Tipouser1;
+			echo form_open('user/registro');
 
-<!--  inicio formulario   -->
-<?php
-	echo $Tipouser1;
-	echo form_open('user/registro');
+			echo form_error('nombre');
+			echo form_label('Nombre','nombre');echo'<br>';
+			
+			
+			echo form_error('contra');
+			echo form_label('Contraseña','contra');echo'<br>';
+			
+			
+			echo form_error('ccontra');
+			echo form_label('Confirma contraseña','ccontra');echo'<br>';
+			
+			
+			echo form_error('correo');
+			echo form_label('Correo','correo');echo'<br>';
+			
+			
+			echo form_error('estado');
+			echo form_label('Estado','estado');echo'<br>';
+			
+			
+			
+		?>
+	</div>
 
-	echo form_error('nombre');
-	echo form_label('Nombre','nombre');
-	echo form_input('nombre'); echo'<br><br>';
-	
-	echo form_error('contra');
-	echo form_label('Contraseña','contra');
-	echo form_password('contra'); echo'<br><br>';
-	
-	echo form_error('ccontra');
-	echo form_label('Confirma contraseña','ccontra');
-	echo form_password('ccontra'); echo'<br><br>';
-	
-	echo form_error('correo');
-	echo form_label('Correo','correo');
-	echo form_input('correo'); echo'<br><br>';
-	
-	echo form_error('estado');
-	echo form_label('Estado','estado');
-	echo form_input('estado'); echo'<br><br>';
-	
-	echo form_label('Tipo', 'tipo');echo'<br>';
-
-	echo form_error('Tipouser1');
-	echo form_label('Comprador', 'comprador');
-	echo form_checkbox('Tipouser1','acceptar', TRUE);echo'<br>';
-	echo form_label('Vendedor', 'vendedor');
-	echo form_checkbox('Tipouser2','acceptar', FALSE);echo'<br>';
-	echo form_label('Colaorador', 'colaborador');
-	echo form_checkbox('Tipouser3','acceptar', FALSE);echo'<br><br>';
-	
-	echo form_submit('registro','Registro');
-
-	echo form_close();
-?>
+	<div class="col-md-3">
+		<?php
+		echo form_input('nombre'); echo'<br>';
+		echo form_password('contra'); echo'<br>';
+		echo form_password('ccontra'); echo'<br>';
+		echo form_input('correo'); echo'<br>';
+		echo form_input('estado'); echo'<br>';
 
 
-</body>
+		?>
 
-</html>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-md-3 col-md-offset-8">
+		<?php
+			echo form_label('Tipo', 'tipo');echo'<br>';
+
+			echo form_error('Tipouser1');
+			echo form_label('Comprador', 'comprador');
+			echo form_checkbox('Tipouser1','acceptar', TRUE);echo'<br>';
+			echo form_label('Vendedor', 'vendedor');
+			echo form_checkbox('Tipouser2','acceptar', FALSE);echo'<br>';
+			echo form_label('Colaorador', 'colaborador');
+			echo form_checkbox('Tipouser3','acceptar', FALSE);echo'<br><br>';
+			
+			echo form_submit('registro','Registro');
+
+			echo form_close();
+		?>
+	</div>
+</div>
